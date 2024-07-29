d7d6323cd373943bb056d3877b2cc41eb569c26b
Nikhil Mekle <nikhilmekle2810@gmail.com>
2024-07-30 01:36:53 +0530

    css changes

--- src/Component/TextForm.js
+++ src/Component/TextForm.js
@@ -68,9 +68,9 @@ export default function TextUtils() {
 
     return (
         <div>   {showAlert && <Alert message={alertMessage} alertType={alertType }/>}
-            <div className="container" style={{ height: "550px", marginTop: "70px", borderRadius: "10px", border: "1px solid black" }}>
-                <h2></h2>
-                <div className="row" style={{ marginTop: "20px", display: "flex", alignItems: "center" }}>
+            <div className="container" style={{ height: "auto",width:'auto', marginTop: "70px", borderRadius: "10px", border: "1px solid black", padding:'10px 10px 30px 10px'}}>
+                
+                <div className="row" style={{ marginTop: "20px", display: "flex", alignItems: "center", gap:'50px'}}>
                     <div className="col-2">
                         <h6 style={{ textAlign: "center", fontWeight: "normal" }}>Total Words</h6>
                         <h2 id="totalWords" style={{ textAlign: "center" }}>{totalWords}</h2>
@@ -85,7 +85,7 @@ export default function TextUtils() {
                     </div>
                 </div>
                 <hr></hr>
-                <div className="row">
+                <div className="row" style={{gap:'50px'}}>
                     <div className="col-8">
                         <textarea
                             className="form-control"
@@ -96,8 +96,8 @@ export default function TextUtils() {
                             style={{ backgroundColor: "rgb(255, 255, 255)", height: "380px", marginLeft: "20px", fontSize: "25px", border: "2px solid black" }}
                         ></textarea>
                     </div>
-                    <div className="col-3" style={{ display: "flex", flexDirection: "column", gap: "40px", marginLeft: "50px", marginTop: "20px" }}>
-                        <button className="btn btn-primary" onClick={upperCaseClick} style={{ height: "50px", fontSize: "17px", fontWeight: "bold" }}>Convert To UpperCase</button>
+                    <div className="col-3" style={{backgroundColor:'black', display: "flex", flexDirection: "column", gap: "40px", width:'auto' , height:'auto', marginLeft:'30px'}}>
+                        <button className="btn btn-primary" onClick={upperCaseClick} style={{ height: "50px" ,fontSize: "17px", fontWeight: "bold" , marginTop: "20px"}}>Convert To UpperCase</button>
                         <button className="btn btn-primary" onClick={lowerCaseClick} style={{ height: "50px", fontSize: "17px", fontWeight: "bold" }}>Convert To LowerCase</button>
                         <button className="btn btn-primary" onClick={copyClick} style={{ height: "50px", fontSize: "17px", fontWeight: "bold" }}>Copy</button>
                         <button className="btn btn-primary" onClick={clearClick} style={{ height: "50px", fontSize: "17px", fontWeight: "bold" }}>Clear</button>
